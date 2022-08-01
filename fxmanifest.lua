fff995a5a50b8169eb24599618a0514892699c3e
Lakindu Kariyawasam <lakindu1997@gmail.com>
2022-08-01 18:03:27 +0530

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+author "LakinduK"
+description "Free Clean LoadingScreen"
+
+files {
+    'index.html',
+    'main.css',
+    'assets/gta610.jpg',
+    'assets/1.jpg',
+    'assets/2.jpg',
+    'assets/3.jpg',
+    'assets/4.jpg',
+    'assets/5.jpg',
+    'assets/discord.png',
+    'assets/fivem.png',
+    'assets/rockstar.png',
+    'assets/gtav.png',
+    'assets/yt.png',
+    'assets/main.js',
+    'assets/slideshow.css',
+    'assets/gravity.woff2',
+    'music/song.mp3',
+    'config.js'
+}
+
+loadscreen 'index.html'
